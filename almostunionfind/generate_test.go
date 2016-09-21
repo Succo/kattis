94ab5bc2cb755df238bbe22e96715750b77d0cdb
Fabrice <fabrice.vaillant@student.ecp.fr>
2016-09-21 11:19:07 +0200

    New version even faster, still too slow

---
+++ almostunionfind/generate_test.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math/rand"
+	"os"
+	"testing"
+	"time"
+)
+
+const (
+	size  = 100000
+	query = 100000
+	cases = 20
+)
+
+func TestRandom(t *testing.T) {
+	rand.Seed(time.Now().UnixNano())
+	var prints, unions, moves time.Duration
+	file, _ := os.Open(os.DevNull)
+	out := bufio.NewWriter(file)
+	for c := 0; c < cases; c++ {
+		s := &sets{make([]int, size), make([][]int, size), out}
+		for i := range s.list {
+			s.list[i] = i
+			s.sets[i] = []int{i}
+		}
+		for i := 0; i < query; i++ {
+			var com, p, q int
+			com = rand.Intn(3) + 1
+			p = rand.Intn(size) + 1
+			q = rand.Intn(size) + 1
+			if com == 1 {
+				now := time.Now()
+				s.union(p, q)
+				unions += time.Since(now)
+			} else if com == 2 {
+				now := time.Now()
+				s.move(p, q)
+				moves += time.Since(now)
+			} else if com == 3 {
+				now := time.Now()
+				s.output(p)
+				prints += time.Since(now)
+			}
+		}
+	}
+	fmt.Printf("unions %s\n", unions.String())
+	fmt.Printf("moves %s\n", moves.String())
+	fmt.Printf("prints %s\n", prints.String())
+}
--- almostunionfind/main.go
+++ almostunionfind/main.go
@@ -8,6 +8,7 @@ import (
 
 type sets struct {
 	list []int
+	sets [][]int
 	out  *bufio.Writer
 }
 
@@ -17,13 +18,15 @@ func main() {
 	for {
 		var n, m int
 		fmt.Fscanln(in, &n, &m)
-		s := &sets{make([]int, n), out}
+		s := &sets{make([]int, n), make([][]int, n), out}
 		for i := range s.list {
 			s.list[i] = i
+			s.sets[i] = []int{i}
 		}
 		for i := 0; i < m; i++ {
 			var com, p, q int
 			fmt.Fscanln(in, &com, &p, &q)
+			//fmt.Printf("%d %d %d\n", com, p, q)
 			if com == 1 {
 				s.union(p, q)
 			} else if com == 2 {
@@ -31,6 +34,7 @@ func main() {
 			} else if com == 3 {
 				s.output(p)
 			}
+			//fmt.Println(s.sets, s.list)
 		}
 		if in.Buffered() == 0 {
 			break
@@ -41,25 +45,56 @@ func main() {
 func (s *sets) union(p, q int) {
 	setp := s.list[p-1]
 	setq := s.list[q-1]
-	for key, set := range s.list {
-		if set == setp {
-			s.list[key] = setq
-		}
+	if setp == setq {
+		return
 	}
+
+	for _, val := range s.sets[setp] {
+		s.list[val] = setq
+	}
+
+	set := make([]int, len(s.sets[setq])+len(s.sets[setp]))
+	copy(set, s.sets[setq])
+	copy(set[len(s.sets[setq]):], s.sets[setp])
+	s.sets[setq] = set
+
+	s.sets[setp] = make([]int, 0)
 }
 
 func (s *sets) move(p, q int) {
-	s.list[p-1] = s.list[q-1]
+	setp := s.list[p-1]
+	setq := s.list[q-1]
+
+	if setp == setq {
+		return
+	}
+
+	s.list[p-1] = setq
+	s.sets[setq] = append(s.sets[setq], p-1)
+
+	if len(s.sets[setp]) == 1 {
+		s.sets[setp] = make([]int, 0)
+		return
+	}
+
+	var pIndex int
+	for i, val := range s.sets[setp] {
+		if val == p-1 {
+			pIndex = i
+			break
+		}
+	}
+
+	s.sets[setp][pIndex] = s.sets[setp][len(s.sets[setp])-1]
+	s.sets[setp] = s.sets[setp][:len(s.sets[setp])-1]
 }
 
 func (s *sets) output(p int) {
 	var sum, len int
 	setp := s.list[p-1]
-	for key, set := range s.list {
-		if set == setp {
-			sum += key + 1
-			len++
-		}
+	for _, val := range s.sets[setp] {
+		sum += val + 1
+		len++
 	}
 	fmt.Fprintf(s.out, "%d %d\n", len, sum)
 	s.out.Flush()
